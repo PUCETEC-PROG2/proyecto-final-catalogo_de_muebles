19f96fe8791213c9b37e25eeb8e781570ea04d42
Arm4nd7 <armando.estevezt@gmail.com>
2025-01-21 23:59:11 -0500

    Modificain de base de datos

--- catalogo_muebles/mueblesemae/urls.py
+++ catalogo_muebles/mueblesemae/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:mueble_id>/", views.mueble, name="mueble"),
     path("cliente/<int:cliente_id>/", views.cliente_details, name="cliente_details")
+    
 ]
 
