b3660c903ff022ccff6fb90a75d1bd5a9cfdd590
Elizabeth Mites <elizabethmites2703@gmail.com>
2025-02-15 00:18:35 -0500

    Version Fimal Proyecto

--- catalogo_muebles/catalogo_muebles/settings.py
+++ catalogo_muebles/catalogo_muebles/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mueblesemae',
         'USER': 'postgres',
-        'PASSWORD': '123456789',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
